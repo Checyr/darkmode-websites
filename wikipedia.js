50231452ff07d7de47e0ba884bacb42cfb4cc060
Checyr <gabrielly.checyr@gmail.com>
2024-11-28 20:57:08 -0300

    Wikipedia darkmode finished; Hacker News still in progress

---
+++ wikipedia.js
@@ -0,0 +1,17 @@
+//
+document.getElementsByTagName('body')[0].style.setProperty('--background-color-base', '#292c2f');
+document.getElementsByTagName('body')[0].style.background = '#292c2f';
+document.getElementsByTagName('body')[0].style.setProperty('--background-color-neutral-subtle', '#292c2f');
+document.getElementsByTagName('body')[0].style.setProperty('--background-color-interactive-subtle', '#292c2f');
+document.getElementsByTagName('body')[0].style.setProperty('--color-progressive', '#f17ea1');
+document.getElementsByTagName('body')[0].style.setProperty('--color-emphasized', '#F1F3F5');
+
+
+// Hacker News - Not Finished Yet 
+document.getElementById('hnmain').style.background = "#292c2f";
+
+const links = document.getElementsByTagName('a');
+
+for(let link of links) {
+    link.style.color = "#F1F3F5";
+}
